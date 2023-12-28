fdd128a57e52d146a0825122f82c293c6c11f487
chee0101 <cheecheese0101@gmail.com>
2023-12-28 22:23:46 +0800

    Add About Content - Vision and Mission ; Add on Comment

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,52 +1,56 @@
 const About = () => {
   return (
-    <div
-      id="about"
-      className="bg-dark-blue text-white pt-12 pb-12 lg:py-[60px] px-7 lg:px-[80px] xl:px-[100px]"
-    >
-      <div className="lg:grid grid-cols-8 gap-8 items-center">
-        {/* Upper Video for smaller screens (hidden for larger screens) */}
-        <div className="hidden lg:block lg:col-span-4 lg:order-1">
-          <div className="relative w-full h-0" style={{ paddingTop: '56.25%' }}>
-            <iframe 
-              src="https://www.youtube.com/embed/jDLuJLoaA_g?si=lQUc0oCiB6zaDWf0" 
-              title="YouTube video player" 
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-              allowFullScreen
-              className="absolute top-0 left-0 w-full h-full"
-            ></iframe>
-          </div>
-        </div>
-        
-        {/* Text */}
-        <div className="lg:col-span-4 lg:order-2 lg:col-start-5">
-          <div className="text-left">
-            <p className="pb-4 text-sm leading-snug md:text-[20px] xl:text-[22px] lg:pt-6 lg:pb-8 text-light-blue font-bold">
-              ABOUT US
-            </p>
-            <h1 className="text-xl leading-snug md:text-[30px] xl:text-[38px] font-bold">
-              Elevate Your Expectation
-            </h1>
-            <p className="pt-3 pb-4 text-sm leading-snug md:text-[16px] xl:text-[18px] lg:pt-6 lg:pb-8">
-              Discover a realm of tech excellence. We are <strong>InnovateXperience</strong>, your partner in shaping the future of digital solutions. With a passion for pushing boundaries, we embark on a journey to revolutionize user experiences.
-            </p>
-          </div>
-        </div>
-        
-        {/* Lower Video for larger screens (hidden for smaller screens) */}
-        <div className="lg:hidden lg:col-span-4 lg:order-3 lg:col-start-1">
-          <div className="relative w-full h-0" style={{ paddingTop: '56.25%' }}>
-            <iframe 
-              src="https://www.youtube.com/embed/jDLuJLoaA_g?si=lQUc0oCiB6zaDWf0" 
-              title="YouTube video player" 
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-              allowFullScreen
-              className="absolute top-0 left-0 w-full h-full"
-            ></iframe>
+    <>
+      <div id="about">
+
+        {/* Video and Description */}
+        <div className="bg-dark-blue text-white pt-12 pb-12 lg:py-[60px] px-7 lg:px-[80px] xl:px-[100px]">
+          <div className="lg:grid grid-cols-8 gap-8 items-center">
+
+            {/* Upper Video for smaller screens (hidden for larger screens) */}
+            <div className="hidden lg:block lg:col-span-4 lg:order-1">
+              <div className="relative w-full h-0" style={{ paddingTop: '56.25%' }}>
+                <iframe 
+                  src="https://www.youtube.com/embed/jDLuJLoaA_g?si=lQUc0oCiB6zaDWf0" 
+                  title="YouTube video player" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                  allowFullScreen
+                  className="absolute top-0 left-0 w-full h-full"
+                ></iframe>
+              </div>
+            </div>
+            
+            {/* Description */}
+            <div className="lg:col-span-4 lg:order-2 lg:col-start-5">
+              <div className="text-left">
+                <p className="pb-4 text-sm leading-snug md:text-[20px] xl:text-[22px] lg:pt-6 lg:pb-8 text-light-blue font-bold">
+                  ABOUT US
+                </p>
+                <h1 className="text-xl leading-snug md:text-[30px] xl:text-[38px] font-bold">
+                  Elevate Your Expectation
+                </h1>
+                <p className="pt-3 pb-4 text-sm leading-snug md:text-[16px] xl:text-[18px] lg:pt-6 lg:pb-8">
+                  Discover a realm of tech excellence. We are <strong>InnovateXperience</strong>, your partner in shaping the future of digital solutions. With a passion for pushing boundaries, we embark on a journey to revolutionize user experiences.
+                </p>
+              </div>
+            </div>
+            
+            {/* Lower Video for larger screens (hidden for smaller screens) */}
+            <div className="lg:hidden lg:col-span-4 lg:order-3 lg:col-start-1">
+              <div className="relative w-full h-0" style={{ paddingTop: '56.25%' }}>
+                <iframe 
+                  src="https://www.youtube.com/embed/jDLuJLoaA_g?si=lQUc0oCiB6zaDWf0" 
+                  title="YouTube video player" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                  allowFullScreen
+                  className="absolute top-0 left-0 w-full h-full"
+                ></iframe>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ frontend/src/components/VisionMission.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import vision from "../images/vision.png";
+import mission from "../images/mission.png";
+import logo from "../images/company-logo.png";
+
+const VisionMission = () => {
+  return (
+    <>
+      <div className="bg-white">
+        <div className="mt-12 mb-12">
+          <div className="flex flex-col w-full lg:flex-row items-center justify-center">
+
+            {/* Mission */}
+            <div className="card card-normal w-full sm:w-80 h-85 lg:w-96 lg:h-96 bg-base-100 shadow-xl border-[3px] border-light-blue lg:p-3 flex-shrink-0 mx-4 mb-4 justify-center items-center">
+              <div className="justify-center">
+
+                <figure>
+                  <img 
+                    src={mission} 
+                    alt="Mission image" 
+                    className="w-[18%] lg:w-[22%] sm:pt-[10px] lg:pt-[0px]" 
+                  />
+                </figure>
+
+                <div className="card-body items-center text-center" style={{ paddingTop: '1rem', paddingBottom: '1rem' }}>
+                  <h2 className="card-title text-[18px] lg:text-[20px]" style={{ marginBottom: '0' }}>Our Mission</h2>
+                  <div className="text-justify">
+                    <p className="md:text-[14px] lg:text-[16px] center">
+                      We lead <strong>technological advancement</strong>, driving innovation across industries. Committed to being pioneers, we relentlessly <strong>pursue cutting-edge solutions</strong> to shape the trajectory of progress.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            {/* Divider with Logo */}
+            <div className="divider divider-info lg:divider-horizontal justify-center mx-10 md:px-10 lg:px-0" style={{ paddingTop: '10px', paddingBottom: '25px' }}>
+              <figure>
+                <img src={logo} alt="Logo image" className="justify-center w-20 sm:w-18 md:w-22 lg:w-30" />
+              </figure>
+            </div>
+
+            {/* Vision */}
+            <div className="card card-normal w-full sm:w-80 h-85 lg:w-96 lg:h-96 bg-base-100 shadow-xl border-[3px] border-light-blue lg:p-3 flex-shrink-0 mx-4 mb-4 justify-center items-center">
+              <div className="justify-center">
+
+                <figure>
+                  <img 
+                    src={vision} 
+                    alt="Vision image" 
+                    className="w-[18%] lg:w-[22%] py-[10px] sm:pt-[15px] sm:pb-[5px]" />
+                </figure>
+
+                <div className="card-body items-center text-center" style={{ paddingTop: '1rem', paddingBottom: '1rem' }}>
+                  <h2 className="card-title text-[18px] lg:text-[24px]" style={{ marginBottom: '0' }}>Our Vision</h2>
+                  <div className="text-justify">
+                    <p className="md:text-[14px] lg:text-[16px] center">
+                      Our vision is a seamlessly <strong>enhanced world</strong> where technology elevates every aspect of life. Join us on a transformative journey into a future where technology is a <strong>harmonious force, connecting and empowering</strong> societies.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default VisionMission;
