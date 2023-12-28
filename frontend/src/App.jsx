69bb8ac966f548b798ef19a9a078bd9348768848
qiujun1230 <qiujunng@gmail.com>
2023-12-28 12:28:35 +0800

    add social media logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,8 @@
 import Home from "./pages/Home.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
-import Service from "./components/Service.jsx"
-import Team from "./components/Team.jsx"
 import Service from "./components/Service.jsx";
+import Team from "./components/Team.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
--- frontend/src/components/Team.jsx
+++ frontend/src/components/Team.jsx
@@ -15,48 +15,132 @@ const Team = () => {
             <h1 className="text-[48px] text-center text-black font-bold">Meet the Innovators</h1>
           </div>
         
-        <div className="mt-12 mb-12 mx-12 flex flex-row space-x-4 justify-center">
+          <div className="mt-12 mb-12 mx-12 flex flex-row gap-8 justify-center">
             
-            <div className="card w-48 bg-base-100 shadow-xl border-[3px] border-light-blue flex-shrink-0 mx-4 mb-4">
+            <div className="card w-68 bg-base-100 shadow-xl border-[3px] border-light-blue flex-shrink-0">
               <figure><img src={YY} alt="Profile Picture of Yu Yuan" /></figure>
               <div className="card-body text-bold text-center">
-                <h2 className="card-title card">Chin Yu Yuan</h2>
-                <p className="text-center">Chief Technology Officer</p>
+                <h2 className="card-title card text-[18px]">Chin Yu Yuan</h2>
+                <p className="text-center mb-5">Chief Technology Officer</p>
                 <div className="card-actions justify-center">
-                  <button className="btn btn-primary">LinkedIn</button>
+                  <nav className="md:place-self-center md:justify-self-end">
+                    <div className="grid grid-flow-col gap-4">
+                      <a
+                        href="https://www.linkedin.com"
+                        className="bg-light-blue w-8 h-8 rounded-full flex items-center justify-center cursor-pointer hover:shadow-md transition ease-in-out duration-300 hover:translate-y-1"
+                      >
+                        <i className="fa-brands fa-linkedin-in fa-lg text-white"></i>
+                      </a>
+                      <a
+                        href="https://www.twitter.com"
+                        className="bg-light-blue w-8 h-8 rounded-full flex items-center justify-center cursor-pointer hover:shadow-md transition ease-in-out duration-300 hover:translate-y-1"
+                      >
+                        <i className="fa-brands fa-x-twitter fa-lg text-white"></i>
+                      </a>
+                      <a
+                        href="https://www.facebook.com"
+                        className="bg-light-blue w-8 h-8 rounded-full flex items-center justify-center cursor-pointer hover:shadow-md transition ease-in-out duration-300 hover:translate-y-1"
+                      >
+                        <i className="fa-brands fa-facebook-f fa-lg text-white"></i>
+                      </a>
+                    </div>
+                  </nav>
                 </div>
               </div>
             </div>
 
-            <div className="card w-48 bg-base-100 shadow-xl border-[3px] border-light-blue flex-shrink-0 mx-4 mb-4">
+            <div className="card w-68 bg-base-100 shadow-xl border-[3px] border-light-blue flex-shrink-0">
               <figure><img src={JW} alt="Profile Picture of Jia Wen" /></figure>
               <div className="card-body text-bold text-center">
-                <h2 className="card-title card">Naomi Tham Kah Mun</h2>
-                <p className="text-center">Chief Executive Officer</p>
+                <h2 className="card-title card text-[18px]">Naomi Tham Kah Mun</h2>
+                <p className="text-center mb-5">Chief Executive Officer</p>
                 <div className="card-actions justify-center">
-                  <button className="btn btn-primary">LinkedIn</button>
+                <nav className="md:place-self-center md:justify-self-end">
+                    <div className="grid grid-flow-col gap-4">
+                      <a
+                        href="https://www.linkedin.com"
+                        className="bg-light-blue w-8 h-8 rounded-full flex items-center justify-center cursor-pointer hover:shadow-md transition ease-in-out duration-300 hover:translate-y-1"
+                      >
+                        <i className="fa-brands fa-linkedin-in fa-lg text-white"></i>
+                      </a>
+                      <a
+                        href="https://www.twitter.com"
+                        className="bg-light-blue w-8 h-8 rounded-full flex items-center justify-center cursor-pointer hover:shadow-md transition ease-in-out duration-300 hover:translate-y-1"
+                      >
+                        <i className="fa-brands fa-x-twitter fa-lg text-white"></i>
+                      </a>
+                      <a
+                        href="https://www.facebook.com"
+                        className="bg-light-blue w-8 h-8 rounded-full flex items-center justify-center cursor-pointer hover:shadow-md transition ease-in-out duration-300 hover:translate-y-1"
+                      >
+                        <i className="fa-brands fa-facebook-f fa-lg text-white"></i>
+                      </a>
+                    </div>
+                  </nav>
                 </div>
               </div>
             </div>
 
-            <div className="card w-48 bg-base-100 shadow-xl border-[3px] border-light-blue flex-shrink-0 mx-4 mb-4">
+            <div className="card w-68 bg-base-100 shadow-xl border-[3px] border-light-blue flex-shrink-0">
               <figure><img src={KC} alt="Profile Picture of Ke Chee" /></figure>
               <div className="card-body text-bold text-center">
-                <h2 className="card-title card">Ching Ke Chee</h2>
-                <p className="text-center">Chief Experience Officer</p>
+                <h2 className="card-title card text-[18px]">Ching Ke Chee</h2>
+                <p className="text-center mb-5">Chief Experience Officer</p>
                 <div className="card-actions justify-center">
-                  <button className="btn btn-primary">LinkedIn</button>
+                <nav className="md:place-self-center md:justify-self-end">
+                    <div className="grid grid-flow-col gap-4">
+                      <a
+                        href="https://www.linkedin.com"
+                        className="bg-light-blue w-8 h-8 rounded-full flex items-center justify-center cursor-pointer hover:shadow-md transition ease-in-out duration-300 hover:translate-y-1"
+                      >
+                        <i className="fa-brands fa-linkedin-in fa-lg text-white"></i>
+                      </a>
+                      <a
+                        href="https://www.twitter.com"
+                        className="bg-light-blue w-8 h-8 rounded-full flex items-center justify-center cursor-pointer hover:shadow-md transition ease-in-out duration-300 hover:translate-y-1"
+                      >
+                        <i className="fa-brands fa-x-twitter fa-lg text-white"></i>
+                      </a>
+                      <a
+                        href="https://www.facebook.com"
+                        className="bg-light-blue w-8 h-8 rounded-full flex items-center justify-center cursor-pointer hover:shadow-md transition ease-in-out duration-300 hover:translate-y-1"
+                      >
+                        <i className="fa-brands fa-facebook-f fa-lg text-white"></i>
+                      </a>
+                    </div>
+                  </nav>
                 </div>
               </div>
             </div>
 
-            <div className="card w-48 bg-base-100 shadow-xl border-[3px] border-light-blue flex-shrink-0 mx-4 mb-4">
+            <div className="card w-68 bg-base-100 shadow-xl border-[3px] border-light-blue flex-shrink-0">
               <figure><img src={QJ} alt="Profile Picture of Qiu Jun" /></figure>
               <div className="card-body text-bold text-center">
-                <h2 className="card-title card">Ng Qiu Jun</h2>
-                <p className="text-center">Chief Operations Officer</p>
+                <h2 className="card-title card text-[18px]">Ng Qiu Jun</h2>
+                <p className="text-center mb-5">Chief Operations Officer</p>
                 <div className="card-actions justify-center">
-                  <button className="btn btn-primary">LinkedIn</button>
+                <nav className="md:place-self-center md:justify-self-end">
+                    <div className="grid grid-flow-col gap-4">
+                      <a
+                        href="https://www.linkedin.com"
+                        className="bg-light-blue w-8 h-8 rounded-full flex items-center justify-center cursor-pointer hover:shadow-md transition ease-in-out duration-300 hover:translate-y-1"
+                      >
+                        <i className="fa-brands fa-linkedin-in fa-lg text-white"></i>
+                      </a>
+                      <a
+                        href="https://www.twitter.com"
+                        className="bg-light-blue w-8 h-8 rounded-full flex items-center justify-center cursor-pointer hover:shadow-md transition ease-in-out duration-300 hover:translate-y-1"
+                      >
+                        <i className="fa-brands fa-x-twitter fa-lg text-white"></i>
+                      </a>
+                      <a
+                        href="https://www.facebook.com"
+                        className="bg-light-blue w-8 h-8 rounded-full flex items-center justify-center cursor-pointer hover:shadow-md transition ease-in-out duration-300 hover:translate-y-1"
+                      >
+                        <i className="fa-brands fa-facebook-f fa-lg text-white"></i>
+                      </a>
+                    </div>
+                  </nav>
                 </div>
               </div>
             </div>
