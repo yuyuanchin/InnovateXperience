b38871513ef9cf8f3700b5f6c3c47735ea36aab3
Yu Yuan <yychin2910@gmail.com>
2023-12-29 14:46:52 +0800

    Refactor code for Footer and Navbar

--- frontend/src/components/Footer/FooterCompany.jsx
+++ frontend/src/components/Footer/FooterCompany.jsx
@@ -5,31 +5,31 @@ const FooterCompany = () => {
     <>
       <nav>
         <header className="footer-title">Company</header>
-        <a className="link link-hover">
+        <div className="link link-hover">
           <Link to="home" smooth={true} duration={500}>
             Home
           </Link>
-        </a>
-        <a className="link link-hover">
+        </div>
+        <div className="link link-hover">
           <Link to="services" smooth={true} duration={500}>
             Services
           </Link>
-        </a>
-        <a className="link link-hover">
+        </div>
+        <div className="link link-hover">
           <Link to="about" smooth={true} duration={500}>
             About Us
           </Link>
-        </a>
-        <a className="link link-hover">
+        </div>
+        <div className="link link-hover">
           <Link to="why-us" smooth={true} duration={500}>
             Why Us
           </Link>
-        </a>
-        <a className="link link-hover">
+        </div>
+        <div className="link link-hover">
           <Link to="team" smooth={true} duration={500}>
             Our Team
           </Link>
-        </a>
+        </div>
       </nav>
     </>
   );
--- frontend/src/components/Navbar/NavbarMenuItems.jsx
+++ frontend/src/components/Navbar/NavbarMenuItems.jsx
@@ -3,31 +3,31 @@ import { Link, animateScroll as scroll } from "react-scroll";
 const NavbarMenuItems = () => {
   return (
     <>
-      <Link to="home" smooth={true} duration={500}>
-        <li>
-          <a>HOME</a>
-        </li>
-      </Link>
-      <Link to="services" smooth={true} duration={500}>
-        <li>
-          <a>SERVICES</a>
-        </li>
-      </Link>
-      <Link to="about" smooth={true} duration={500}>
-        <li>
-          <a>ABOUT</a>
-        </li>
-      </Link>
-      <Link to="why-us" smooth={true} duration={500}>
-        <li>
-          <a>WHY US</a>
-        </li>
-      </Link>
-      <Link to="team" smooth={true} duration={500}>
-        <li>
-          <a>TEAM</a>
-        </li>
-      </Link>
+      <li>
+        <Link to="home" smooth={true} duration={500}>
+          HOME
+        </Link>
+      </li>
+      <li>
+        <Link to="services" smooth={true} duration={500}>
+          SERVICES
+        </Link>
+      </li>
+      <li>
+        <Link to="about" smooth={true} duration={500}>
+          ABOUT
+        </Link>
+      </li>
+      <li>
+        <Link to="why-us" smooth={true} duration={500}>
+          WHY US
+        </Link>
+      </li>
+      <li>
+        <Link to="team" smooth={true} duration={500}>
+          TEAM
+        </Link>
+      </li>
     </>
   );
 };
