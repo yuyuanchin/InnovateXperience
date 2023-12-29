8ee17946054e125c2fb275d7c57b93ec5cace36a
Yu Yuan <yychin2910@gmail.com>
2023-12-29 20:26:08 +0800

    Refactor code

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -14,17 +14,25 @@ const Services = () => {
     });
   }, []);
 
-  {/* Card for Service Info */}
+  {
+    /* Card for Service Info */
+  }
   const Card = ({ image, title, description }) => (
     <div className="card card-normal w-80 h-85 lg:w-96 lg:h-96 bg-base-100 shadow-xl border-[3px] border-light-blue lg:p-3 flex-shrink-0 mx-4 mb-4 justify-center items-center min-h-[327.4px]">
       <div className="pt-5 justify-center">
-
         {/* Service Icon */}
         <figure>
-          <img src={image} alt={`${title} image`} className="w-[20%] lg:w-[24%]"/>
+          <img
+            src={image}
+            alt={`${title} image`}
+            className="w-[20%] lg:w-[24%]"
+          />
         </figure>
 
-        <div className="card-body items-center text-center" style={{paddingTop: '1rem'}} >
+        <div
+          className="card-body items-center text-center"
+          style={{ paddingTop: "1rem" }}
+        >
           <h2 className="card-title text-[20px] lg:text-[22px]">{title}</h2>
           <div className="text-justify">
             {/* Service Description */}
@@ -41,7 +49,6 @@ const Services = () => {
     <>
       <div id="services" className="bg-white">
         <div className="mt-12 mb-12">
-
           {/* Section Title */}
           <div className="pt-3 pb-3">
             <h1 className="md:text-[22px] xl:text-[24px] text-center text-light-blue font-bold">
@@ -51,21 +58,25 @@ const Services = () => {
 
           {/* Section Description */}
           <div className="pb-6">
-            <h1 className="sm:text-[25px] md:text-[30px] xl:text-[40px] text-center text-black font-bold">We are Providing Digital Services</h1>
+            <h1 className="sm:text-[25px] md:text-[30px] xl:text-[40px] text-center text-black font-bold">
+              We are Providing Digital Services
+            </h1>
           </div>
 
-        <div className="flex flex-wrap justify-center gap-4 mx-auto">
-          {data.map((service) => (
-            <Card
-              key={service.id}
-              image={service.imageUrl}
-              title={service.title}
-              description={service.description}
-            />
-          ))}
+          {/* Services */}
+          <div className="flex flex-wrap justify-center gap-4 mx-auto">
+            {data.map((service) => (
+              <Card
+                key={service.id}
+                image={service.imageUrl}
+                title={service.title}
+                description={service.description}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
